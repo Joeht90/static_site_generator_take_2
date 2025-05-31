849f6880548719204fda3e0cc6947a9b0c724887
joeht90 <joeht90@icloud.com>
2025-05-31 11:07:13 -0700

    Notes on how to continue the inline markdown function

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -3,13 +3,16 @@ from textnode import TextNode, TextType
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
-        if text_type == TextType.TEXT:
-            new_nodes.extend(old_node)
+        if text_type != TextType.TEXT:
+            new_nodes.append(old_node)
         else:
-            split_list = old_node.split(delimiter, 2)
-            new_nodes.extend(TextNode(split_list[0], text_type=TextType.TEXT))
-            new_nodes.extend(TextNode(split_list[1], text_type))
-            new_nodes.extend(TextNode(split_list[2], TextType.TEXT))
+            split_list = old_node.text.split(delimiter, 2)
+            # I dont have time to finish this before lunch is over but
+            # you can use the index() method to return the index of the list
+            # anything between the delimiter will be within an odd number index
+            # and anything even will be of TextType.TEXT
+            new_nodes.append(TextNode(split_list[1], text_type))
+            new_nodes.append(TextNode(split_list[2], TextType.TEXT))
     return new_nodes
 
-#testing
+
